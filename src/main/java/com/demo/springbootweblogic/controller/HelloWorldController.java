759c7e78ccecddd3a266f73aa79ac91bc53fb313
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2018-11-29 22:36:38 +0700

    Add log4j usage example, Fix not bundle log4j jar

--- src/main/java/com/demo/springbootweblogic/controller/HelloWorldController.java
+++ src/main/java/com/demo/springbootweblogic/controller/HelloWorldController.java
@@ -3,14 +3,20 @@ package com.demo.springbootweblogic.controller;
 
 import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	private static Logger logger = LogManager.getLogger(HelloWorldController.class);
+
 	@GetMapping("/helloworld")
 	ArrayList<String> readResource() {
+
+		logger.info("hello world log4j");
 
 		ArrayList<String> examples = new ArrayList<>();
 		examples.add("Hello");
