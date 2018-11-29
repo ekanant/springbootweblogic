3173bc1dd998717aea95170b2f4796bae6a6411d
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2018-11-29 23:29:03 +0700

    Add annotaion example

--- src/main/java/com/demo/springbootweblogic/controller/HelloWorldController.java
+++ src/main/java/com/demo/springbootweblogic/controller/HelloWorldController.java
@@ -6,7 +6,16 @@ import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+/*
+*	Example Reference
+*	https://dzone.com/articles/using-the-spring-requestmapping-annotation
+*/
 
 @RestController
 public class HelloWorldController {
@@ -26,5 +35,69 @@ public class HelloWorldController {
 		examples.add("demo");
 		examples.add("project");
 		return examples;
+	}
+
+	@RequestMapping(value = "/anotherhelloworld", method = RequestMethod.GET)
+	ArrayList<String> anotherReadResource() {
+		logger.info("anotherReadResource");
+		ArrayList<String> examples = new ArrayList<>();
+		examples.add("This");
+		examples.add("is");
+		examples.add("anotherReadResource");
+		return examples;
+	}
+
+	@RequestMapping(value = "/multirequestmethods", method = { RequestMethod.GET, RequestMethod.POST })
+	ArrayList<String> multiRequestMethods() {
+
+		logger.info("multiRequestMethods");
+
+		ArrayList<String> examples = new ArrayList<>();
+		examples.add("This");
+		examples.add("is");
+		examples.add("multi");
+		examples.add("request");
+		examples.add("methods");
+		return examples;
+	}
+
+	/**
+	 * If Request content type is not application/json or application/xml, request
+	 * will be rejected
+	 * 
+	 */
+	@RequestMapping(value = "/cons", consumes = { "application/JSON", "application/XML" })
+	String getConsumes() {
+		return "Consumes attribute";
+	}
+
+	@RequestMapping(value = "/fetch", params = { "personId=10" })
+	String getParams(@RequestParam("personId") String id) {
+		logger.info("getParams, id=>" + id);
+		return "Work onlt personID=10, Fetched parameter using params attribute = " + id;
+	}
+
+	@RequestMapping(value = "/fetch/{id}", method = RequestMethod.GET)
+	String getDynamicUriValue(@PathVariable String id) {
+		logger.info("ID is " + id);
+		return "Dynamic URI parameter fetched";
+	}
+
+	@RequestMapping(value = "/fetch/{id:[a-z]+}/{name}", method = RequestMethod.GET)
+	String getDynamicUriValueRegex(@PathVariable("name") String name) {
+		logger.info("Name is " + name);
+		return "Dynamic URI parameter fetched using regex";
+	}
+
+	@RequestMapping(value = "/getById")
+	String getIdByValue(@RequestParam("id") String personId) {
+		logger.info("ID is " + personId);
+		return "Get ID from query string of URL with value element";
+	}
+
+	@RequestMapping(value = "/getByPersonId")
+	String getId(@RequestParam String personId) {
+		logger.info("ID is " + personId);
+		return "Get ID from query string of URL without value element";
 	}
 }
