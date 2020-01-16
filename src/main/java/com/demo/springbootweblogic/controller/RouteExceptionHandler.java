8bf16682b659441ee8735bb936ea53dcd6a43d5c
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-01-16 14:05:58 +0700

    Add handler for missing request parameter

--- src/main/java/com/demo/springbootweblogic/controller/RouteExceptionHandler.java
+++ src/main/java/com/demo/springbootweblogic/controller/RouteExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -71,6 +72,20 @@ public class RouteExceptionHandler extends ResponseEntityExceptionHandler {
                 json.addProperty("timestamp", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(new Date()));
                 json.addProperty("message", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON_UTF8).body(json.toString());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status,  WebRequest request) {
+        HttpServletRequest servletRequest = ((ServletWebRequest)request).getRequest();
+        String requestUrl = getFullURL(servletRequest);
+
+        logger.error("request_url=>"+requestUrl, ex);
+
+        JsonObject json = new JsonObject();
+        json.addProperty("request_url", requestUrl);
+        json.addProperty("timestamp", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(new Date()));
+        json.addProperty("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON_UTF8).body(json.toString());
     }
 
     @ExceptionHandler({ Exception.class })
