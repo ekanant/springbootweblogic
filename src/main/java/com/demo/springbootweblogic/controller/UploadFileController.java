0a59014adc866f3a24b6e9c94931b84eff62c734
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-10-30 09:55:32 +0700

    decode formdata with utf8 char

--- src/main/java/com/demo/springbootweblogic/controller/UploadFileController.java
+++ src/main/java/com/demo/springbootweblogic/controller/UploadFileController.java
@@ -1,22 +1,18 @@
 package com.demo.springbootweblogic.controller;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @RestController
 @RequestMapping(value = "/uploadfile")
@@ -24,13 +20,18 @@ class UploadFileController {
 
     private static Logger logger = LogManager.getLogger(UploadFileController.class);
 
-    @PostMapping("/upload")
-    Map<String, Object> uploadFile(@RequestParam("file") MultipartFile file) {
+    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    Map<String, Object> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam(name = "fileName") String filenameEnc) {
+        
+        String filenameDec = new String(filenameEnc.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+
         Map<String, Object> resp = new HashMap<>();
         try {
             resp.put("parameterName", file.getName());
             resp.put("originalFileName", file.getOriginalFilename());
             resp.put("fileSize", file.getSize());
+            resp.put("filename_param_original", filenameEnc);
+            resp.put("filename_param_decode", filenameDec);
             // Get the file and save it somewhere
             // byte[] bytes = file.getBytes();
             // Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
